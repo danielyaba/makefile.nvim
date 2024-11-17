675c04763163888e447b26f6d72e2a75f0e3c171
Daniel Yaba <daniely@digital.gov.il>
2024-11-17 12:58:49 +0200

    Added setup function to plugin/makefile_nvim.lua

--- plugin/makefile_nvim.lua
+++ plugin/makefile_nvim.lua
@@ -1,1 +1,12 @@
-require("makefile_nvim").setup()
+-- plugin/makefile_nvim.lua
+local M = {}
+
+function M.setup()
+  -- Your plugin setup code goes here
+  -- For example: defining commands, keymaps, or anything related to setting up the plugin
+  vim.api.nvim_create_user_command("MakefileUI", function()
+    require("makefile_nvim.ui").show_makefile_ui()
+  end, {})
+end
+
+return M
